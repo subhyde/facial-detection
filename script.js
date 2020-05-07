0bbc3f63f0a04c40aebe9600cdb226c6c40075e9
ethan <ejohnson16@myseneca.ca>
2020-05-07 00:11:00 -0400

    fixed bugs

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const video = document.getElementById('video');
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('/models')
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+    faceapi.nets.ageGenderNet.loadFromUri('./models')
 
 ]).then(startVideo)
 function startVideo(){
@@ -22,17 +23,20 @@ video.addEventListener('play', () =>{
     const displaySize = {  width: video.width, height: video.height }
     faceapi.matchDimensions(canvas,displaySize)
    setInterval(async () =>{
-       const detections = await  faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions()).withAgeAndGender().withFaceExpressions().withFaceExpressions()
-       const resizedDetections = faceapi.resizeResults(detections,displaySize)
-       //makes sure the box gets deleted every time a new one spawns
-       canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
-       faceapi.draw.drawFaceExpressions(canvas,resizedDetections)
+       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+       const resizedDetections = faceapi.resizeResults(detections, displaySize)
+       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+       faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
-       //draws the age and gender of the user
+
+
        resizedDetections.forEach( detection => {
            const box = detection.detection.box
            const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+           console.log(detection.age)
            drawBox.draw(canvas)
        })
+
    },100)
+
 })
